8c9459eb2bf4e98d3bb99b01224c77c5a76b5901
Jaewoo Kim <wodndhksk@gmail.com>
2023-02-05 18:41:01 +0900

    데이터베이스 연결, 연결 확인 테스트

---
+++ src/test/java/hello/jdbc/connection/DBConnectionUtilTest.java
@@ -0,0 +1,15 @@
+package hello.jdbc.connection;
+
+import org.junit.jupiter.api.Test;
+import java.sql.Connection;
+import static org.assertj.core.api.Assertions.*;
+
+class DBConnectionUtilTest {
+
+    @Test
+    void connection(){
+        Connection connection = DBConnectionUtil.getConnection();
+        assertThat(connection).isNotNull();
+    }
+
+}
