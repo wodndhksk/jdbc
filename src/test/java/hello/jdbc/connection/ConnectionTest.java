84a71cc617d4d2f8ef76778ef82ddba10458356c
Jaewoo Kim <wodndhksk@gmail.com>
2023-02-25 23:12:32 +0900

    DataSource 예제1 - DriverManager

---
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -0,0 +1,30 @@
+package hello.jdbc.connection;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+
+@Slf4j
+public class ConnectionTest {
+
+    @Test
+    void driverManager() throws SQLException {
+        Connection con1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        Connection con2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        log.info("connection={}, class={}", con1, con1.getClass());
+        log.info("connection={}, class={}", con2, con2.getClass());
+    }
+
+    @Test
+    void dataSourceDriverManager(){
+        //DriverManagerDataSource 항상 새로운 커넥션을 획득
+        DataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+    }
+}
