129f2523246f45a5da350ee207cae00c34db3903
Jaewoo Kim <wodndhksk@gmail.com>
2023-02-26 16:26:33 +0900

    DataSource 예제2 - 커넥션 풀

--- src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package hello.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -15,7 +16,7 @@ import static hello.jdbc.connection.ConnectionConst.*;
 public class ConnectionTest {
 
     @Test
-    void driverManager() throws SQLException {
+    void dataSource() throws SQLException {
         Connection con1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         Connection con2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         log.info("connection={}, class={}", con1, con1.getClass());
@@ -23,8 +24,32 @@ public class ConnectionTest {
     }
 
     @Test
-    void dataSourceDriverManager(){
+    void dataSourceDriverManager() throws SQLException {
         //DriverManagerDataSource 항상 새로운 커넥션을 획득
         DataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        useDataSource(dataSource);
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        //커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("Mypool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000);
+
+    }
+
+    @Test
+    void useDataSource(DataSource dataSource) throws SQLException {
+        Connection con1 = dataSource.getConnection();
+        Connection con2 = dataSource.getConnection();
+        log.info("connection={}, class={}", con1, con1.getClass());
+        log.info("connection={}, class={}", con2, con2.getClass());
     }
 }
