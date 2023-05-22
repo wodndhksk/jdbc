3db331eda2eaf93bff87a7016b9ba9623aa5138a
Jaewoo Kim <wodndhksk@gmail.com>
2023-05-22 17:02:11 +0900

    refactor: 스프링 예외 추상화 이해 - 사용하지 않는 주석 삭제

--- src/test/java/hello/jdbc/exception/translator/SpringExceptionTranslatorTest.java
+++ src/test/java/hello/jdbc/exception/translator/SpringExceptionTranslatorTest.java
@@ -1,23 +1,20 @@
 package hello.jdbc.exception.translator;
 
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
-import org.springframework.jdbc.support.SQLExceptionTranslator;
 
 import javax.sql.DataSource;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import static hello.jdbc.connection.ConnectionConst.*;
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 public class SpringExceptionTranslatorTest {
@@ -54,7 +51,7 @@ public class SpringExceptionTranslatorTest {
             PreparedStatement stmt = con.prepareStatement(sql);
             stmt.executeQuery();
 
-        } catch (SQLException e){
+        } catch (SQLException e) {
             assertThat(e.getErrorCode()).isEqualTo(42122);
             SQLErrorCodeSQLExceptionTranslator exTranslator = new SQLErrorCodeSQLExceptionTranslator(dataSource);
 
